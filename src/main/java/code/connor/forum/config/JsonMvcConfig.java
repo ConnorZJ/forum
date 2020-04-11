0ac0f226698f6e598e1fb4b01bfad678ce6ab4f6
connorZJ <1020379482@qq.com>
2020-04-11 22:38:07 +0800

    json配置

---
+++ src/main/java/code/connor/forum/config/JsonMvcConfig.java
@@ -0,0 +1,11 @@
+package code.connor.forum.config;
+
+import org.springframework.beans.factory.annotation.Configurable;
+
+/**
+ * created by Connor Xiong
+ */
+@Configurable
+public class JsonMvcConfig {
+
+}
