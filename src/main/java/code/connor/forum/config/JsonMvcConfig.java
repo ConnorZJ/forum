1b37d2ab54604d2e1327669aad13788adda15bdd
connorZJ <1020379482@qq.com>
2020-04-13 00:20:36 +0800

    json配置

--- src/main/java/code/connor/forum/config/JsonMvcConfig.java
+++ src/main/java/code/connor/forum/config/JsonMvcConfig.java
@@ -1,11 +1,26 @@
 package code.connor.forum.config;
 
-import org.springframework.beans.factory.annotation.Configurable;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+
+import java.text.SimpleDateFormat;
 
 /**
  * created by Connor Xiong
  */
-@Configurable
+@Configuration
 public class JsonMvcConfig {
+
+    @Bean
+    MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd"));
+        converter.setObjectMapper(objectMapper);
+        return converter;
+    }
 
 }
