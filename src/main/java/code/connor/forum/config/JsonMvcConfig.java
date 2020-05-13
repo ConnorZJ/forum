4227a1986b65ff7563b5af530822b9022ecb317c
ConnorZJ <1020379482@qq.com>
2020-05-13 23:41:42 +0800

    将Jackson换成fastjson的配置

--- src/main/java/code/connor/forum/config/JsonMvcConfig.java
+++ src/main/java/code/connor/forum/config/JsonMvcConfig.java
@@ -1,26 +1,45 @@
 package code.connor.forum.config;
 
 
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.io.Serializable;
+import java.nio.charset.Charset;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 /**
- * created by Connor Xiong
+ * @Description FastJson配置
+ * @Author Connor Xiong
+ * @Date 2020-05-13 22:04
  */
 @Configuration
-public class JsonMvcConfig {
+public class JsonMvcConfig implements WebMvcConfigurer {
 
-    @Bean
-    MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
-        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd"));
-        converter.setObjectMapper(objectMapper);
-        return converter;
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        FastJsonHttpMessageConverter fastJsonHttpMessageConverter = new FastJsonHttpMessageConverter();
+        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        fastJsonConfig.setCharset(Charset.defaultCharset());
+        fastJsonConfig.setSerializerFeatures(SerializerFeature.WriteNullListAsEmpty,
+                SerializerFeature.WriteNullStringAsEmpty,
+                SerializerFeature.WriteMapNullValue);
+        List<MediaType> mediaTypes = new ArrayList<>();
+        mediaTypes.add(MediaType.APPLICATION_JSON);
+        // 将Jackson的消息转换器移除，不然会默认使用
+        converters.removeIf(next -> next instanceof MappingJackson2HttpMessageConverter);
+        converters.add(fastJsonHttpMessageConverter);
+
     }
-
 }
