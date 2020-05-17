ce6bcc51c9d36939e131dd4f1c00ed11ee3a30bf
ConnorZJ <1020379482@qq.com>
2020-05-17 20:45:09 +0800

    modify

--- src/main/java/code/connor/forum/config/JsonMvcConfig.java
+++ src/main/java/code/connor/forum/config/JsonMvcConfig.java
@@ -4,19 +4,14 @@ package code.connor.forum.config;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.io.Serializable;
 import java.nio.charset.Charset;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 /**
