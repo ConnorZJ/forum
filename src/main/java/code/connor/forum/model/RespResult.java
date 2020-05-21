5b437cd56081c4e8ccbe1f785d9de987c04216a3
ConnorZJ <1020379482@qq.com>
2020-05-21 23:46:04 +0800

    增加commong-langs3工具包

---
+++ src/main/java/code/connor/forum/model/RespResult.java
@@ -0,0 +1,9 @@
+package code.connor.forum.model;
+
+/**
+ * @Description TODO
+ * @Author Connor Xiong
+ * @Date 2020-05-21 23:34
+ */
+public class RespResult {
+}
---
+++ src/main/java/code/connor/forum/util/RespResultUtil.java
@@ -0,0 +1,9 @@
+package code.connor.forum.util;
+
+/**
+ * @Description TODO
+ * @Author Connor Xiong
+ * @Date 2020-05-21 23:36
+ */
+public class RespResultUtil {
+}
