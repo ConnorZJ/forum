783e5287f2eb3853a487e9f526cc7cb22271ec63
connorzj <1020379482@qq.com>
2020-06-02 00:24:28 +0800

    init

---
+++ forum-vue/src/utils/api.js
@@ -0,0 +1,55 @@
+import axios from 'axios'
+import {
+  Message
+} from 'element-ui'
+
+axios.interceptors.response.use(success => {
+  if (success.code == '0') {
+    return success;
+  }
+}, error => {
+  if (error.response.status == 504 || error.response.status == 404) {
+    Message.error({
+      message: "去不了你想要去的地方喔！"
+    });
+  } else if (error.response.status == 403) {
+    Message.error({
+      message: "权限不足，请联系小熊~"
+    });
+  } else if (error.response.status == 401) {
+    Message.error({
+      message: "请先登录~"
+    });
+  } else {
+    if (error.response.data.msg) {
+      Message.error({
+        message: error.response.data.msg
+      });
+    } else {
+      Message.error({
+        message: "请求失败，请稍后重试！"
+      });
+    }
+  }
+  return;
+})
+
+let base = '';
+
+export const post = (url, params) => {
+  return axios({
+    method: 'post',
+    data: params,
+    url: `${base}${url}`,
+    transformRequest: [function (e) {
+      let r = '';
+      for (let i in e) {
+        r += encodeURIComponent(i) + '=' + encodeURIComponent(e[i]) + '&'
+      }
+      return r;
+    }],
+    headers: {
+      "Context-Type": "application/x-www-form-urlencoded"
+    }
+  });
+}
---
+++ forum-vue/src/views/Login.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="loginForm">
+    <el-form :model="loginForm" ref="loginForm" :rules="rules">
+      <h3>请登录!</h3>
+      <el-form-item label="用户名" prop="username">
+        <el-input type="text" v-model="loginForm.username" auto-complete="false"></el-input>
+      </el-form-item>
+      <el-form-item label="密码" prop="password">
+        <el-input type="password" v-model="loginForm.password" auto-complete="false"></el-input>
+      </el-form-item>
+      <el-button type="primary" @click="onLogin">登录</el-button>
+    </el-form>
+  </div>
+</template>
+<script>
+import { post } from '../utils/api'
+import axios from 'axios'
+export default {
+  name: 'Login',
+  data() {
+    return {
+      loginForm: {
+        username: '',
+        password: ''
+      },
+      rules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' }
+        ],
+        password: [{ required: true, message: '请输入密码', trigger: 'blur' }]
+      }
+    }
+  },
+  methods: {
+    onLogin() {
+      this.$refs.loginForm.validate(valid => {
+        if (valid) {
+          post('/doLogin', this.loginForm).then(res => {})
+        } else {
+          this.$message.error('请输入用户名或密码')
+          return false
+        }
+      })
+    }
+  }
+}
+</script>
+<style scoped>
+.loginForm {
+  width: 50%;
+  text-align: center;
+  border-radius: 15px;
+  background-clip: padding-box;
+  margin: 180px auto;
+  padding: 35px 35px 15px 35px;
+  border: 1px solid #eaeaea;
+  box-shadow: 0 0 25px #cac6c6;
+}
+</style>
