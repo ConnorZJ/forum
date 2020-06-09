ebf483600e84706109e1dcaa366328b0dbc49dcf
connorzj <1020379482@qq.com>
2020-06-09 23:05:44 +0800

    注册

--- forum-backend/src/main/java/code/connor/forum/controller/LoginController.java
+++ forum-backend/src/main/java/code/connor/forum/controller/LoginController.java
@@ -1,8 +1,13 @@
 package code.connor.forum.controller;
 
 import code.connor.forum.model.RespResult;
+import code.connor.forum.model.User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @Description
@@ -15,6 +20,12 @@ public class LoginController {
     @GetMapping("/login")
     public RespResult login() {
         return RespResult.fail("尚未登录");
+    }
+
+    @PostMapping("/register")
+    public RespResult register(HttpServletRequest request, @RequestBody User user) {
+        System.out.println(user);
+        return RespResult.success();
     }
 
 }
